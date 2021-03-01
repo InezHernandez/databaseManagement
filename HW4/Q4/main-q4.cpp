f9b2d47f2bbaaf17bbb5204c914ac72271de3f6a
eggersja <eggersja@oregonstate.edu>
2021-02-28 17:31:55 -0800

    repo reorganizing

---
+++ HW4/Q4/main-q4.cpp
@@ -0,0 +1,14 @@
+// Description: External Memory Sorting
+// Authors: Jackson E and Inez Hernandez F
+
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+using std::cout;
+
+
+int main() {
+	cout << "Hello world!\n";
+}
